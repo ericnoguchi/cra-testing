831ebd33067c76ba129c44d63d8a37629ae62097
Eric Noguchi <eric.noguchi@logmein.com>
2020-04-16 15:23:32 -0400

    redux first router

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import Link from 'redux-first-router-link';
 import './App.css';
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Link to="/users/1">DB & GRAPHQL</Link> <br />
+      <Link to={['users', '2']}>REACT & REDUX</Link> <br />
+      <Link to={{ type: 'USERS', payload: { id: '3' } }}>FP</Link>
+      <br />
+      <Link to={{ type: 'USERS' }}>Main</Link>
+      <div
+        onClick={() => {
+          dispatch({ type: 'USERS', payload: { id: '4' } });
+        }}
+      >
+        4 using normal dispatch
+      </div>
+      <br />
+      <div>
+        <div>Queue settings page {}</div>
+      </div>
+      <div>
+        Users List
+        <div>Users Profile page {}</div>
+      </div>
+      <div>General settings</div>
     </div>
   );
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,30 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {
+  Action,
+  configureStore,
+  getDefaultMiddleware,
+  ThunkAction,
+} from '@reduxjs/toolkit';
+import { connectRoutes } from 'redux-first-router';
+import createHashHistory from 'history/createHashHistory';
 
+const routesMap = {
+  HOME: '/',
+
+  USERS: '/users/:id?',
+  QUEUES: '/queues/:id?',
+};
+
+const { reducer, middleware, enhancer } = connectRoutes(routesMap, {
+  createHistory: createHashHistory,
+});
+
+const middlewares = [...getDefaultMiddleware(), middleware];
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    location: reducer,
   },
+  middleware: middlewares,
+  enhancers: [enhancer],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
